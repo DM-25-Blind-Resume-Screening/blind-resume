7c9cd934757acf0d545e9fa3166a89dadb89bc62
Christopher Wilson <cmw.wilson88@gmail.com>
2017-08-30 12:55:07 -0600

    Finish userRoutes

--- public/src/routes/userRoutes.js
+++ public/src/routes/userRoutes.js
@@ -1,6 +1,7 @@
 import UserSidebar from '../components/MainLayout/Sidebars/UserSidebar/UserSidebar.vue'
 import JobListings from '../components/MainLayout/ContentSections/UserContent/JobListings/JobListings.vue'
 import LongJobPost from '../components/MainLayout/ContentSections/UserContent/JobPostInfo/LongJobPost.vue'
+import ResumeInfo from '../components/MainLayout/ContentSections/UserContent/BuildProfileResume/ResumeInfo.vue'
 
 export const userRoutes = [
 	{
@@ -11,10 +12,17 @@ export const userRoutes = [
 		}
 	},
 	{
+		path: 'user/:user_id/resume',
+		components: {
+			sidebar: UserSidebar,
+			content: ResumeInfo
+		}
+	},
+	{
 		path: 'user/:user_id/:job_post_id',
 		components: {
 			sidebar: UserSidebar,
 			content: LongJobPost
 		}
-	}
+	},
 ]
