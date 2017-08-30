f26eb035bc25cf32ff266fed91efb5785a92dbaa
Ashley Schuette <ashleyschuette2@gmail.com>
2017-08-30 15:52:38 -0600

    Typewriter installed

---
+++ public/src/components/MainLayout/ContentSections/UserContent/JobListings/JobList.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      <app-short-job-post></app-short-job-post>
+  </div>
+</template>
+
+<script>
+import ShortJobPost from './ShortJobPost.vue';
+
+export default {
+  components: {
+      appShortJobPost: ShortJobPost
+  }
+}
+</script>
+
