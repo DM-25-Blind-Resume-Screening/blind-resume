77227df91bff63a00d2d94ad943c94181006867d
Logan Truong <ttluan411@gmail.com>
2017-08-30 14:49:29 -0600

    for Ashley

--- public/src/components/Home/Landing.vue
+++ public/src/components/Home/Landing.vue
@@ -1,18 +1,25 @@
 <template>
 	<div>
+		<app-nav-bar></app-nav-bar>
+		<app-landing-picture></app-landing-picture>
 		<app-home-banner></app-home-banner>
+
 	</div>
 </template>
 
 <script>
 	import HomeBanner from './LandingElements/HomeBanner.vue';
+	import NavBar from './LandingElements/NavBar.vue';
+	import LandingPicture from './LandingElements/LandingPicture.vue'
 	export default {
 	    components: {
-	      appHomeBanner: HomeBanner
+	      appHomeBanner: HomeBanner,
+				appLandingPicture: LandingPicture,
+				appNavBar: NavBar
 	    }
 	}
 </script>
 
 <style>
-	
+
 </style>
---
+++ public/src/components/Home/LandingElements/Navbar.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="navbar_div">
+    <div class="nav">
+      LOGIN
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.navbar_div {
+  width: 100vw;
+  height: 60px;
+  background: linear-gradient(45deg,#87e2f2,#4473ff);
+  display: flex;
+  justify-content: flex-end;
+  box-shadow: 1px 1px 10px 4px rgba(0,0,0,0.5);
+  position:fixed;
+  top: 0;
+  opacity: 0.9;
+  z-index: 999;
+}
+.navbar_div .nav {
+  width: 100px;
+  height: 30px;
+  background: white;
+  margin: auto 40px;
+  font-weight: 400;
+  font-size: 15px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
