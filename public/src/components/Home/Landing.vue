c3704a596a149e6c791b12bfd79b7a7f634b5d58
Logan Truong <ttluan411@gmail.com>
2017-08-30 18:16:20 -0600

    added article

--- public/src/components/Home/Landing.vue
+++ public/src/components/Home/Landing.vue
@@ -3,19 +3,21 @@
 		<app-nav-bar></app-nav-bar>
 		<app-landing-picture></app-landing-picture>
 		<app-home-banner></app-home-banner>
-
+		<app-why-we-exist></app-why-we-exist>
 	</div>
 </template>
 
 <script>
 	import HomeBanner from './LandingElements/HomeBanner.vue';
 	import NavBar from './LandingElements/NavBar.vue';
-	import LandingPicture from './LandingElements/LandingPicture.vue'
+	import LandingPicture from './LandingElements/LandingPicture.vue';
+	import WhyWeExist from './LandingElements/WhyWeExist.vue';
 	export default {
 	    components: {
 	      appHomeBanner: HomeBanner,
 				appLandingPicture: LandingPicture,
-				appNavBar: NavBar
+				appNavBar: NavBar,
+				appWhyWeExist: WhyWeExist
 	    }
 	}
 </script>
--- public/src/components/Home/LandingElements/WhyWeExist.vue
+++ public/src/components/Home/LandingElements/WhyWeExist.vue
@@ -1,6 +1,15 @@
 <template>
-  <div class="">
-    <h1>Why we exist</h1>
+  <div class="why-wrapper">
+    <div class="header-div">
+      <div class="line"></div>
+      <h1>Why we exist</h1>
+      <div class="line"></div>
+    </div>
+    <p>Studies did something </p>
+    <div class="article-div">
+      <img class="article" src="../../../assets/article.png" alt="article">
+    </div>
+
   </div>
 </template>
 
@@ -9,5 +18,36 @@ export default {
 }
 </script>
 
-<style lang="css">
+<style>
+.why-wrapper {
+  height: 700px;
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  flex-direction: column;
+  background: pink;
+}
+.header-div {
+  height: 50px;
+  width: 90%;
+  font-size: 25px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+.line{
+  height:1px;
+  width: 40%;
+  background: black;
+}
+.article-div {
+  height: 400px;
+  width: 500px;
+}
+.article {
+  height:400px;
+  width:500px;
+  box-shadow: 1px 1px 15px rgba(0, 0, 0, 0.5);
+}
 </style>
