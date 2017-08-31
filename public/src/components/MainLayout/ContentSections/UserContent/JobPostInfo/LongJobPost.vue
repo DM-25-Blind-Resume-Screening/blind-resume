26ee51eeb7dd2755060bd6e8d31d50f4fc6cb2ea
Ashley Schuette <ashleyschuette2@gmail.com>
2017-08-31 12:18:12 -0600

    Complete LongJob and ShortJob feeds for user views

--- public/src/components/MainLayout/ContentSections/UserContent/JobPostInfo/LongJobPost.vue
+++ public/src/components/MainLayout/ContentSections/UserContent/JobPostInfo/LongJobPost.vue
@@ -1,11 +1,38 @@
 <template>
 	<div>
 		<app-default-header></app-default-header>
-		<h1>Company</h1>
-		<h3>Description</h3>
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti quia cumque consequatur voluptates necessitatibus? Odio vitae animi ipsam saepe, totam. Sequi voluptate beatae nihil nostrum. Molestiae quas veniam incidunt eum.</p>
-		<h3>Responsibilities</h3>
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro veniam sequi sint iure architecto omnis, incidunt natus est temporibus quibusdam! Doloribus magnam vero suscipit quam provident consequuntur, nam totam cumque?</p>
+		<div class="company-info-section">
+			<div>
+				<img width="120px" src="http://certifiedeurocollision.com/wp-content/uploads/2015/11/bmw-logo.png">
+			</div>
+			<div class="company-info-content">
+				<h1 class="ljp-h1">Company Info</h1>
+				<p class="ljp-p"> Stuff about company</p>
+			</div>
+		</div>
+
+		<div class="ljp-job-info-section">
+			<h2 class="ljp-h2">Senior Front End Devloper</h2>
+		<div class="ljp-job-info-content">
+			<h3 class="ljp-h3">Job Description</h3>
+				<p class="ljp-p">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti quia 
+					cumque consequatur voluptates necessitatibus? Odio vitae animi ipsam saepe, totam. Sequi 
+					voluptate beatae nihil nostrum. Molestiae quas veniam incidunt eum.</p>
+			<h3 class="ljp-h3">Responsibilities</h3>
+				<p class="ljp-p">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro veniam 
+					sequi sint iure architecto omnis, incidunt natus est temporibus quibusdam! Doloribus 
+					magnam vero suscipit quam provident consequuntur, nam totam cumque?</p>
+			<h3 class="ljp-h3">Key Qualifications</h3>
+				<p class="ljp-p">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro veniam 
+					sequi sint iure architecto omnis, incidunt natus est temporibus quibusdam! Doloribus 
+					magnam vero suscipit quam provident consequuntur, nam totam cumque?</p>			
+		</div>
+		<div class="ljp-btns-container">
+			<button class="ljp-save-job-btn">Save Job</button>
+			<button class="ljp-apply-btn">Apply</button>
+		</div>
+		</div>
+		
 	</div>
 </template>
 
@@ -21,5 +48,83 @@ export default {
 
 
 <style>
-	
+
+.company-info-section {
+	display: flex;
+	margin: 60px 60px;
+}
+
+.company-info-content {
+	margin-left: 60px;
+}
+
+.ljp-job-info-section {
+	margin-left: 60px;
+	width: 880px;
+}
+
+.ljp-job-info-content {
+	width: 880px;
+}
+
+.ljp-h1 {
+	font-size: 20px;
+	width: 700px;
+	border-bottom: 1.5px solid #313131;
+}
+
+.ljp-h2 {
+	font-size: 17px;
+	width: 880px;
+	border-bottom: 1.5px solid #313131;
+}
+
+.ljp-h3 {
+	font-size: 15px;
+	font-weight: 600;
+	margin-top: 10px;
+}
+
+.ljp-p {
+	margin-top: 10px;
+}
+
+.ljp-btns-container {
+	display: flex;
+	justify-content: flex-end;
+	margin-top: 40px;
+}
+
+.ljp-save-job-btn {
+	width: 110px;
+	height: 30px;
+	font-family: 'Avenir' Arial, Helvetica, sans-serif;
+	color: #313131;
+	font-size: 15px;
+	background: transparent;
+	outline: none;
+	border: 1px solid #3b83bf;
+	margin-right: 20px;
+}
+
+.ljp-save-job-btn:hover {
+	background: #3b83bf;
+	color: #fff;
+	cursor: pointer;
+}
+
+.ljp-apply-btn {
+	width: 110px;
+	height: 30px;
+	font-family: 'Avenir' Arial, Helvetica, sans-serif;
+	color: #fff;
+	font-size: 15px;
+	background: #3b83bf;
+	outline: none;
+	border: none;
+}
+
+.ljp-apply-btn:hover {
+	cursor: pointer;
+}
 </style>
