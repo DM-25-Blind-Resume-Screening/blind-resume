7eaa5602cac47e6a49c9ce2e847c5c86fbd3ff00
Christopher Wilson <cmw.wilson88@gmail.com>
2017-08-31 12:00:49 -0600

    Add config file with massive information.  Begin Schema creation

--- server/index.js
+++ server/index.js
@@ -3,3 +3,15 @@ const bodyParser = require('body-parser'),
 	  massive	 = require('massive'),
 	  cors		 = require('cors'),
 	  app		 = express();
+
+const connectionString = require('../config');
+
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.static(`${__dirname}/../public/dist`))
+
+
+
+const PORT = 3000
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
