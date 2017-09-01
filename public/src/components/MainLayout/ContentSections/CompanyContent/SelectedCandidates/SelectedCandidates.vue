7436f09d1d529bbdc231b97b275754726c96c849
Logan Truong <ttluan411@gmail.com>
2017-09-01 00:15:25 -0600

    first commit

--- public/src/components/MainLayout/ContentSections/CompanyContent/SelectedCandidates/SelectedCandidates.vue
+++ public/src/components/MainLayout/ContentSections/CompanyContent/SelectedCandidates/SelectedCandidates.vue
@@ -1,5 +1,6 @@
 <template>
 	<div class="selected-candidates-display">
+		<app-default-header></app-default-header>
 		<h1 class="sc-header">Selected Candidates</h1>
 		<hr>
 		<app-candidate-list></app-candidate-list>
@@ -7,9 +8,11 @@
 </template>
 
 <script>
+	import DefaultHeader from '../../../Headers/DefaultHeader.vue'
 	import CandidateList from './CandidateList.vue'
 	export default {
 		components: {
+			appDefaultHeader: DefaultHeader,
 			appCandidateList: CandidateList
 		}
 	}
