56b820f80c5604f6cfefe070d13be620acc7ac86
Ashley Schuette <ashleyschuette2@gmail.com>
2017-09-01 00:08:35 -0600

    Add post button to NewJobPost view

--- public/src/components/MainLayout/ContentSections/CompanyContent/JobPost/NewJobPost.vue
+++ public/src/components/MainLayout/ContentSections/CompanyContent/JobPost/NewJobPost.vue
@@ -1,41 +1,58 @@
 <template>
 	<div>
 		<app-content-header>
-			<h1>New Job Post</h1>
+			<h1>Create New Job Post</h1>
 		</app-content-header>
 		<app-job-description class="nj-editor" v-model="newJobPost.jobDescription"></app-job-description>
 		<app-responsibilities class="nj-editor" v-model="newJobPost.jobResponsibilities"></app-responsibilities>
 		<app-keyword-list v-model="newJobPost.jobKeywords"></app-keyword-list>
+		<button class="nj-post-btn">Post job</button>
 	</div>
 </template>
 
 <script>
-	import DefaultHeader from '../../../Headers/DefaultHeader.vue'
-	import JobDescription from './JobPostElements/JobDescription.vue'
-	import Responsibilities from './JobPostElements/Responsibilities.vue'
-	import KeywordList from './JobPostElements/KeywordList.vue'
-	export default {
-		data() {
-			return {
-				newJobPost: {
-					jobDescription: '',
-					jobResponsibilities: '',
-					jobKeywords: []
-				}
+import DefaultHeader from '../../../Headers/DefaultHeader.vue'
+import JobDescription from './JobPostElements/JobDescription.vue'
+import Responsibilities from './JobPostElements/Responsibilities.vue'
+import KeywordList from './JobPostElements/KeywordList.vue'
+export default {
+	data() {
+		return {
+			newJobPost: {
+				jobDescription: '',
+				jobResponsibilities: '',
+				jobKeywords: []
 			}
-		},
-		components: {
-			appContentHeader: DefaultHeader,
-			appJobDescription: JobDescription,
-			appResponsibilities: Responsibilities,
-			appKeywordList: KeywordList
 		}
+	},
+	components: {
+		appContentHeader: DefaultHeader,
+		appJobDescription: JobDescription,
+		appResponsibilities: Responsibilities,
+		appKeywordList: KeywordList
 	}
+}
 </script>
 
 <style>
-	.nj-editor {
-		width: 75%;
-		margin: 20px auto;
-	}
+.nj-editor {
+	width: 75%;
+	margin: 20px auto;
+}
+
+.nj-post-btn {
+	width: 110px;
+	height: 30px;
+	background: transparent;
+	border: 1px solid #3f51b5;
+	font-size: 15px;
+	color: #3f51b5;
+}
+
+.nj-post-btn:hover {
+	background: #3f51b5;
+	color: #fff;
+	cursor: pointer;
+}
+
 </style>
