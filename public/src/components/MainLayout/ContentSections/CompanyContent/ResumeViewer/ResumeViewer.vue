0cf3cbd6ade50892160d9a4527447fadb65c214e
Ashley Schuette <ashleyschuette2@gmail.com>
2017-09-02 16:02:07 -0600

    Resume elements layed out and blur applied

--- public/src/components/MainLayout/ContentSections/CompanyContent/ResumeViewer/ResumeViewer.vue
+++ public/src/components/MainLayout/ContentSections/CompanyContent/ResumeViewer/ResumeViewer.vue
@@ -1,24 +1,56 @@
 <template>
 	<div>
-		<h1>Resume Viewer</h1>
 		<app-resume-list></app-resume-list>
 		<div class="rv-buttons">
-			<button>Save</button><button>Pass</button>
+			<button class="rv-pass-btn">Pass</button>
+			<button class="rv-save-btn">Save</button>
 		</div>
 	</div>
 </template>
 
 <script>
-	import ResumeList from './ResumeList.vue'
-	export default {
-		components: {
-			appResumeList: ResumeList
-		}
+import ResumeList from './ResumeList.vue'
+export default {
+	components: {
+		appResumeList: ResumeList
 	}
+}
 </script>
 
 <style>
-	.rv-buttons {
-		display: flex;
-	}
+.rv-buttons {
+	display: flex;
+	width: 100%;
+	justify-content: center;
+	margin-top: 40px;
+}
+
+.rv-save-btn {
+	width: 130px;
+	height: 35px;
+	outline: none;
+	background: #2baf7a;
+	border-radius: 50px;
+	border: none;
+	text-transform: uppercase;
+	font-size: 17px;
+	font-weight: 500;
+	letter-spacing: 1px;
+	color: #fff;
+	margin-left: 40%;
+}
+
+.rv-pass-btn {
+	width: 130px;
+	height: 35px;
+	outline: none;
+	background: #D55050;
+	border-radius: 50px;
+	border: none;
+	text-transform: uppercase;
+	font-size: 17px;
+	font-weight: 500;
+	letter-spacing: 1px;
+	color: #fff;
+}
 </style>
