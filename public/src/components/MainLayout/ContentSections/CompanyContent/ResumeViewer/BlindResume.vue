488cfe9a534076d20efbb2505a3091a9a1018970
Christopher Wilson <cmw.wilson88@gmail.com>
2017-09-11 15:30:22 -0600

    Add resume viewer front end logic and animation

--- public/src/components/MainLayout/ContentSections/CompanyContent/ResumeViewer/BlindResume.vue
+++ public/src/components/MainLayout/ContentSections/CompanyContent/ResumeViewer/BlindResume.vue
@@ -3,31 +3,36 @@
 		<div>
 			<div class="br-resume-content">
 				<h1 class="br-h1">Work Experience</h1>
-				<ul>
-					<li>Web Developer - DevMountain</li>
-					<li>School Psychologist - School District</li>
-					<li>NASA - Astronaut Intern</li>
-				</ul>
+				<div v-for="experience in resume.resume_work_experience" key="exprience.id">
+					<div class="br-work-header">
+						<h1>{{experience.title}}</h1> - <span>{{experience.company}}</span>
+					</div>
+					<span><em>{{experience.from_date}}</em></span> - <span><em>{{experience.to_date}}</em></span>
+					<p>{{experience.description}}</p>
+					<br>
+				</div>
+				<hr>
 				<h1 class="br-h1">Education</h1>
-				<ul>
-					<li>DevMountain</li>
-					<li>University of Florida</li>
-					<li>NASA Clown College</li> //ahh my school!!!
-				</ul>
-
+				<div v-for="education in resume.resume_education" key="education.id">
+					<h1>{{education.school}}</h1>
+					<h3>{{education.study_field}} - {{education.degree}}</h3>
+					<span>{{education.from_date}}</span> - <span>{{education.to_date}}</span>
+					<p>{{education.description}}</p>
+				</div>
+				<hr>
 				<h1 class="br-h1">Skills</h1>
-				<p>HTMl5, VueJS, Academic Assessments, Cognitive Ability Assessments, Flying a dang space shuttle</p>
-
+				<ul>
+					<li v-for="skill in resume.resume_skills" key="skill.id">{{skill.name}}</li>
+				</ul>
 			</div>
 		</div>
 	</div>
 </template>
 
 <script>
-
-
-export default {
-}
+	export default {
+		props: ['resume']
+	}
 </script>
 
 
@@ -37,10 +42,22 @@ export default {
 	margin-top: 10px;
 }
 
+.br-resume-content hr {
+	width: 50%;
+}
+
 .br-h1 {
 	font-size: 15px;
-	font-weight: 500;
+	font-weight: 700;
+	text-decoration: underline;
 	margin-top: 10px;
+	margin-bottom: 5px;
 }
+
+.br-work-header {
+	display: flex;
+}
+
+
 </style>
 
--- public/src/components/MainLayout/ContentSections/CompanyContent/ResumeViewer/ResumeViewer.vue
+++ public/src/components/MainLayout/ContentSections/CompanyContent/ResumeViewer/ResumeViewer.vue
@@ -17,20 +17,23 @@
 			<button @click="save" class="rv-save-btn">Save</button>
 		</div>
 
-		<div class="resume-container">
-			<div class="display-resume-container">
+		<div class="resume-viewer-container">
+			<div v-if="!resumes.length">Loading...</div>
+			<div v-else>
 				<transition appear 
 							mode="out-in" 
-							v-if="show_box" 
-							enter-active-class="animated slideInRight" 
+							:duration="{enter:1000, leave: 500}"
+							enter-active-class="animated slideInRight"
 							:leave-active-class="leaveClass">
 
-					<app-blind-resume class="displayResume" 
+					<app-blind-resume class="display-resume-container displayResume" 
+							v-if="show_box" 
 							:resume="resumes[currentIndex]" 
-							@click="remove(index)">
+							@click="remove(index)"
+							key="currentIndex">
 					</app-blind-resume>
 
-					<app-blind-resume class="nextResume"></app-blind-resume>
+					<!-- <app-blind-resume key="currentIndex+1" class="nextResume"></app-blind-resume> -->
 
 				</transition>
 			</div>
@@ -42,64 +45,84 @@
 <script>
 import DefaultHeader from '../../../Headers/DefaultHeader.vue'
 import BlindResume from './BlindResume.vue'
+import axios from 'axios'
 export default {
 	data() {
 		return {
 			leaveClass: "animated hinge",
 			show_box: true,
-			resumes: [{id: 1, title: 'WebDev', company: 'DevMtn'},{id: 2, title: 'Sales Assoc', company: "Macy's"}],
+			resumes: [],
 			currentIndex: 0
 		}
 	},
 
 	methods: {
+		getInitialApplications() {
+			return axios.get(`http://localhost:3000/api/job_postings/${this.$route.params.job_post_id}/resumes`)
+						.then(res => {
+							this.resumes = res.data
+						})
+						.catch(err => console.log(err))
+		},
 		pass() {
 			this.leaveClass = "animated hinge";
-			this.next()
-			this.remove()
+			const vm = this;
+			setTimeout(function() {
+				vm.remove()
+			}, 10)
 		},
 		save() {
 			this.leaveClass = "animated bounceOutUp";
-			this.next()
-			this.remove()
+			const vm = this;
+			setTimeout(function() {
+				vm.remove()
+			}, 10)
 		},
 		next() {
-			if (this.currentIndex + 1 < this.numbers.length) {
+			if (this.currentIndex + 1 < this.resumes.length) {
 				this.currentIndex++
 			}
 		},
 		remove(index) {
+			this.show_box = false
 			this.resumes.splice(index, 1);
+			const vm = this
+			setTimeout(function() {
+				vm.show_box = true;
+			}, 2000);
 		}
 	},
 
 	components: {
 		appBlindResume: BlindResume,
 		appDefaultHeader: DefaultHeader
+	},
+	created() {
+		this.getInitialApplications()
 	}
 
 }
 </script>
 
 <style>
-.resume-container {
+.resume-viewer-container {
 	display: flex;
 	align-items: center;
 }
 
 .display-resume-container {
-	width: 100%;
+	width: 60%;
 	display: flex;
 	justify-content: center;
-}
-
-.displayResume {
-	width: 40%;
-	height: 500px;
+	background-color: #fff;
 	margin-top: 30px;
 	box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
 }
 
+/*
+.displayResume {
+	width: 80%;
+*/
 .nextResume {
 	height: 300px;
 	position: absolute;
